ade3c98607151fad2ff3335bf84d74beaba17670
jlegendary <j.legendary2@gmail.com>
2015-08-31 08:40:15 -0400

    Added 007 Solution.java

---
+++ Java/Introduction/007/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution{
+	public static void main(String[] args){
+		Scanner cin=new Scanner(System.in);
+
+		for(int z=1; cin.hasNext(); z++){
+			System.out.println(String.valueOf(z)+" "+cin.nextLine());
+		}
+	}
+}
